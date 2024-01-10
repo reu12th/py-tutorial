89aa9ab8839bf567da594f669cb4a9beaddf58c5
Reuben Abdul <reu12th@gmail.com>
2024-01-10 10:09:07 +0100

    Program to guess a number

--- guess.py
+++ guess.py
@@ -1,31 +1,28 @@
+#!/usr/bin/python3
 from random import randint
 
-def get_input():
 
+def get_input():
     while True:
-        user_input = input("Guess: ")
+        user_input = input("Guess a number: ")
         try:
             return int(user_input)
-        except:
+        except Error:
             print("That's not a number!")
 
 
 def generate_random_number():
-    
     return randint(1, 20)
+
 
 random_number = generate_random_number()
 user_value = -1
-
 for i in range(5):
     user_value = get_input()
     if user_value == random_number:
+        print("Correct!")
         break
+    elif user_value < random_number:
+        print("Go Higher!!!")
     else:
-        if user_value < random_number:
-            print("Go Higher!!!")
-        else:
-            print("Go Lower!!!")
-
-if user_value == random_number:
-    print("Correct!")
+        print("Go Lower!!!")
